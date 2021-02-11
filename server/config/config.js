2c2283324111a3ea15f23df4117ca74414dcdc51
Kelvin Martinez <kmr19972015@gmail.com>
2021-02-11 15:06:41 -0500

    mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -14,10 +14,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 // Base de datos
 // ============================
 let urlDB;
-// if (process.env.NODE_ENV === "dev") {
-//   urlDB = "mongodb://localhost:27017/cafe";
-// } else {
-urlDB = "mongodb+srv://Kelvin:Prueba123@cluster0.g8okg.mongodb.net/test";
-// }
+if (process.env.NODE_ENV === "dev") {
+  urlDB = "mongodb://localhost:27017/cafe";
+} else {
+  urlDB = "mongodb+srv://Kelvin:Prueba123@cluster0.g8okg.mongodb.net/cafe";
+}
 
 process.env.URLDB = urlDB;
